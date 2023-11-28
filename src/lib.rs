f7a92512c6deb5f90c73c092d96d8bfc601de977
jamesavechives <yanhuangpai@126.com>
2023-11-28 14:01:13 +0800

    add test mod for whole process

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod weighted_selection;
 
 fn main() {
     // Example integration
-    let candidates = read_candidates_from_file("Candidates.json")
+    let candidates = read_candidates_from_file("/Library/WebServer/Documents/uvrf/src/Candidates.json")
         .expect("Failed to read candidates");
 
     let (sk, _) = generate_key_pair();
@@ -20,3 +20,36 @@ fn main() {
         println!("No candidate was selected");
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vrf_random_selection_from_file() {
+        let path = "/Library/WebServer/Documents/uvrf/src/Candidates.json"; // Adjust the path to your test JSON file
+        let candidates = read_candidates_from_file(path).expect("Failed to read candidates");
+
+        // Here, use the actual VRF function to generate a random value
+        let (sk, _) = generate_key_pair();
+        let vrf_input = b"test input"; // Use a constant input for reproducibility
+        let vrf_output = generate_random_value_vrf(&sk, vrf_input);
+        let random_number = hash_to_number(&vrf_output);
+        let selected_candidate = choose_candidate_vrf(&candidates, random_number);
+
+        
+        println!("Random number : {}", random_number);
+
+        if let Some(candidate) = selected_candidate {
+            println!("Selected candidate address: {}", candidate.address);
+        } else {
+            println!("No candidate was selected");
+        }
+
+        assert!(selected_candidate.is_some());
+        // You can add more assertions here
+    }
+
+    // ... other tests ...
+}
+
